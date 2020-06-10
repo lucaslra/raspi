eb0b04991577f6dd14c967296d0e63b419bf5141
Lucas Araujo <lucas.de.ribeiroaraujo@asml.com>
2020-06-10 21:10:56 +0200

    Starting the games :)

---
+++ Flasher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Flasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ledPin = 21;
+            var controller = new GpioController();
+            controller.OpenPin(ledPin, PinMode.Output);
+
+            int lightTimeInMs = 500;
+            int dimTimeInMs = 200;
+
+            while (true)
+            {
+                Console.WriteLine($"LED1 on for {lightTimeInMs}ms");
+
+                // turn on the LED
+                controller.Write(ledPin, PinValue.Low);
+                Thread.Sleep(lightTimeInMs);
+                Console.WriteLine($"LED1 off for {dimTimeInMs}ms");
+
+                // turn off the LED
+                controller.Write(ledPin, PinValue.High);
+                Thread.Sleep(dimTimeInMs);
+            }
+        }
+    }
+}
---
+++ Lighter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace Lighter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lightPin = 23;
+            var controller = new GpioController();
+            controller.OpenPin(lightPin, PinMode.Input);
+
+            var state = controller.Read(lightPin);
+
+            while (true)
+            {
+                if (state == controller.Read(lightPin))
+                {
+                    if (controller.Read(lightPin) == PinValue.High)
+                    {
+                        Console.WriteLine("\u263e");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\u263c");
+                    }
+                }
+                state = controller.Read(lightPin);
+
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
